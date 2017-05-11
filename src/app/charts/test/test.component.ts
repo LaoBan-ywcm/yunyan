1118f7a111215ff739cc1d52e1f157199e4ae90f
LaoBan-ywcm <2459007415@qq.com>
2017-05-11 20:59:44 +0800

    测试echarts

---
+++ src/app/charts/test/test.component.ts
@@ -0,0 +1,62 @@
+import {Component, OnInit, OnChanges} from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent  implements OnInit ,OnChanges{
+
+  option:any;
+
+  ngOnInit(){
+    this.option = {
+      tooltip: {
+        trigger: 'item',
+        formatter: "{a} <br/>{b}: {c} ({d}%)"
+      },
+      legend: {
+        orient: 'vertical',
+        x: 'left',
+        data:['未利用','已123']
+      },
+      series: [
+        {
+          name:'访问来源',
+          type:'pie',
+          radius: ['50%', '70%'],
+          avoidLabelOverlap: false,
+          label: {
+            normal: {
+              show: false,
+              position: 'center'
+            },
+            emphasis: {
+              show: true,
+              textStyle: {
+                fontSize: '30',
+                fontWeight: 'bold'
+              }
+            }
+          },
+          labelLine: {
+            normal: {
+              show: false
+            }
+          },
+          data:[
+            {value:400, name:'未利用'},
+            {value:300, name:'已123'},
+          ]
+        }
+      ]
+    };
+  }
+
+  ngOnChanges(){
+  }
+
+}
+
+
+
