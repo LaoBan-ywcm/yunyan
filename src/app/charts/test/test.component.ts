e40c9bef64878dc799929a14ded1ad3750615a2b
LaoBan-ywcm <2459007415@qq.com>
2017-05-11 21:23:22 +0800

    修复angular2-echarts问题

--- src/app/charts/test/test.component.ts
+++ src/app/charts/test/test.component.ts
@@ -1,71 +1,16 @@
-<<<<<<< HEAD
+
 import {Component, OnInit, OnChanges} from '@angular/core';
-=======
-import { Component, OnInit } from '@angular/core';
->>>>>>> origin/master
+
+
 
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.css']
 })
-<<<<<<< HEAD
-export class TestComponent  implements OnInit ,OnChanges{
-
-  option:any;
-
-  ngOnInit(){
-    this.option = {
-      tooltip: {
-        trigger: 'item',
-        formatter: "{a} <br/>{b}: {c} ({d}%)"
-      },
-      legend: {
-        orient: 'vertical',
-        x: 'left',
-        data:['未利用','已123']
-      },
-      series: [
-        {
-          name:'访问来源',
-          type:'pie',
-          radius: ['50%', '70%'],
-          avoidLabelOverlap: false,
-          label: {
-            normal: {
-              show: false,
-              position: 'center'
-            },
-            emphasis: {
-              show: true,
-              textStyle: {
-                fontSize: '30',
-                fontWeight: 'bold'
-              }
-            }
-          },
-          labelLine: {
-            normal: {
-              show: false
-            }
-          },
-          data:[
-            {value:400, name:'未利用'},
-            {value:300, name:'已123'},
-          ]
-        }
-      ]
-    };
-  }
-
-  ngOnChanges(){
-  }
-
-}
 
 
 
-=======
 export class TestComponent  {
   option = {
     backgroundColor: '#123123',
@@ -280,6 +225,76 @@ export class TestComponent  {
     ]
   };
 
+  option4 = {
+    backgroundColor: '#123123',
+
+    title: {
+      text: 'CPU利用率',
+      left: 'center',
+      top: 20,
+      textStyle: {
+        color: '#ccc'
+      }
+    },
+
+    tooltip : {
+      trigger: 'item',
+      formatter: "{a} <br/>{b} : {c} ({d}%)"
+    },
+
+    visualMap: {
+      show: false,
+      min: 80,
+      max: 600,
+      inRange: {
+        colorLightness: [0, 1]
+      }
+    },
+    series : [
+      {
+        name:'访问来源',
+        type:'pie',
+        radius : '55%',
+        center: ['50%', '50%'],
+        data:[
+          {value:200, name:'已利用'},
+          {value:311, name:'未利用'},
+
+        ].sort(function (a, b) { return a.value - b.value; }),
+        roseType: 'radius',
+        label: {
+          normal: {
+            textStyle: {
+              color: 'rgba(255, 255, 255, 0.3)'
+            }
+          }
+        },
+        labelLine: {
+          normal: {
+            lineStyle: {
+              color: 'rgba(255, 255, 255, 0.3)'
+            },
+            smooth: 0.2,
+            length: 10,
+            length2: 20
+          }
+        },
+        itemStyle: {
+          normal: {
+            color: '#c23531',
+            shadowBlur: 200,
+            shadowColor: 'rgba(0, 0, 0, 0.5)'
+          }
+        },
+
+        animationType: 'scale',
+        animationEasing: 'elasticOut',
+        animationDelay: function (idx) {
+          return Math.random() * 200;
+        }
+      }
+    ]
+  };
+
 
 }
->>>>>>> origin/master
